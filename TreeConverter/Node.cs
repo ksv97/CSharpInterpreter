4c42580ce2dfea2ddc9fece0c719b3a68767c187
rp\rodion.kosov <rodion.kosov@akvelon.com>
2020-01-24 21:46:44 +0300

    Add tree converter class and logic

---
+++ TreeConverter/Node.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeConverter
+{
+    public class Node
+    {
+
+        public string Data { get; set; }
+        public Node Parent { get; set; }
+        public List<Node> Children { get; set; }
+
+        public Node(string data, Node parent)
+        {
+            this.Data = data;
+            this.Parent= parent;
+            Children = new List<Node>();
+        }
+
+
+        public void AddChild(Node node)
+        {
+            Children.Add(node);
+        }
+
+        public bool IsNonTerminal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Определяет, содержит ли узел терминал, не несущий никакой семантической нагрузки
+        /// </summary>
+        /// <returns></returns>
+        public bool IsSemanticlessTerminal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasOnlyOneChild()
+        {
+            return Children.Count == 1;
+        }
+
+        public bool HasNonTerminals()
+        {
+            foreach (Node node in Children)
+            {
+                if (node.IsNonTerminal())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Определяет, есть ли у текущего узла дочерний узел, в котором содержится терминал, не несущий никакой семантической нагрузки.
+        /// </summary>
+        /// <returns></returns>
+        public bool HasTerminalChildWithSemanticlessTerminal(out Node nodeWithSemanticlessTerminal)
+        {
+            nodeWithSemanticlessTerminal = null;
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (Children[i].IsNonTerminal())
+                {
+                    continue;
+                }
+                else
+                if (Children[i].IsSemanticlessTerminal())
+                {
+                    nodeWithSemanticlessTerminal = Children[i];
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Определяет, есть ли среди нижестоящих узлов узел, в котором содержится операция, при том что соседние узлы являются операндами.
+        /// </summary>
+        /// <param name="indexOfOperationChildNode"></param>
+        /// <returns></returns>
+        public bool HasOperationChild(out Node operationChildNode)
+        {
+            operationChildNode  = null;
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (Children[i].IsOperation())
+                {
+                    operationChildNode  = Children[i];
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Возвращает самый левый нетерминальный дочерний узел. или <see langword="null"/>.
+        /// </summary>
+        /// <returns></returns>
+        public Node GetLeftmostNonTerminalChild()
+        {
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (Children[i].IsNonTerminal())
+                {
+                    return Children[i];
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ TreeConverter/TreeConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeConverter
+{
+    public static class TreeConverter
+    {
+        static Node currentNode;
+
+        public static void ConvertGrammarTreeToOperationTree(ref Node root)
+        {
+            currentNode = root;
+
+        step1:
+            if (!currentNode.HasNonTerminals())
+                return;
+
+         step2:
+            currentNode = currentNode.GetLeftmostNonTerminalChild();
+
+        step3:
+            if (currentNode.HasOnlyOneChild())
+            {
+                RetargetNode(currentNode);
+                goto step1;
+            }
+            else goto step4;
+
+        step4:
+            if (currentNode.HasTerminalChildWithSemanticlessTerminal(out var semanticlessNode))
+            {
+                currentNode.Children.Remove(semanticlessNode);
+                goto step1;
+            }
+            else goto step5;
+
+        step5:
+            if (currentNode.HasOperationChild(out var operationChildNode))
+            {
+                // todo Здесь может быть проблема, потому что безусловно считаем, что если тут операция, то все остальные символы - операнды
+                currentNode.Data = operationChildNode.Data;
+                currentNode.Children.Remove(operationChildNode);
+                goto step6;
+            }
+
+        step6:
+            if (currentNode.HasNonTerminals())
+            {
+                currentNode = currentNode.GetLeftmostNonTerminalChild();
+                goto step3;
+            }
+            else return;
+        }
+
+        /// <summary>
+        /// Изменяет ссылки на потомков и родителей для текущего узла
+        /// </summary>
+        /// <param name="node"></param>
+        private static void RetargetNode(Node node)
+        {
+            if (node.Children.Count > 1)
+            {
+                throw new Exception("Node contains more than one children. Operation is inconsistent!");
+            }
+
+            foreach (Node node1 in node.Children)
+            {
+                node1.Parent = node;
+            }
+            node.Children = node.Children[0].Children;
+        }
+
+    }
+}
